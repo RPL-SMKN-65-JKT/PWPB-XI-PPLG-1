670998b61496d4b6bedb0ed5a01867c35f492798
Unnecessary <110374556+MuhamadZafarSyah@users.noreply.github.com>
2024-01-20 22:04:28 +0700

    Update index.blade.php
    Mengubah title website

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Muhamad Zafar Syah Portfolio</title>
+        <title>Muhamad Zafar Syah</title>
         <link rel="shortcut icon" href="{{ url('assets//img/icon.png') }}" type="image/x-icon">
 
         <!-- Style  -->
